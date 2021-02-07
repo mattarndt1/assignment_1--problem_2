cd4890ec1ad10607af0ac79b45aecb8be63c9153
mattarndt1 <60207898+mattarndt1@users.noreply.github.com>
2021-02-06 23:32:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,16 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([41.8781, -87.6298], 10);
 
-L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+  // load a tile layer
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
-	minZoom: 1,
-	maxZoom: 16,
-	ext: 'jpg'
-}).addTo(mymap);
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+}).addTo(map);
 
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
-var marker2 = L.marker([51.5105, -0.11]).addTo(mymap);
-
-var circle = L.circle([51.508, -0.11], {
-    color: 'green',
-    fillColor: '#000',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(mymap);
-
-var polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(mymap);
-
-marker.bindPopup("<b>Hello world!</b><br>I am a popup for a marker.").openPopup();
-marker2.bindPopup("<b>Hello world!</b><br>I am a popup for a different marker.");
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
-
-var popup = L.popup();
-
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);
-}
-
-mymap.on('click', onMapClick);
+  // load GeoJSON from an external file
+  $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-python-for-gis-and-rs/master/Week%201/chicago.geojson",function(data){
+    // add GeoJSON layer to the map once the file is loaded
+    L.geoJson(data).addTo(map);
+  });
